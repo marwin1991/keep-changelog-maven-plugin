869849e96cfaeeef1d1808ea9b83a77b6ce3baa3
Peter <peter.zmilczak@gmail.com>
2021-10-19 21:32:44 +0200

    Fixed test

--- src/test/java/com/github/marwin1991/keep_changelog/markdown/MarkdownChangelogTest.java
+++ src/test/java/com/github/marwin1991/keep_changelog/markdown/MarkdownChangelogTest.java
@@ -11,7 +11,7 @@ class MarkdownChangelogTest {
     void disableFormattingAndInspectionTest() {
         //given:
         Changelog changelog = new Changelog();
-        String expectedFirstLine = "<!-- @formatter:off --><!-- noinspection -->";
+        String expectedFirstLine = "<!-- @formatter:off -->\n<!-- noinspection -->";
         MarkdownChangelog markdownChangelog = new MarkdownChangelog(changelog);
 
         //when
